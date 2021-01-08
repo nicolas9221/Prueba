00a5df096f5c5580bf0f16b7bfcc2687765b662c
nicolas9221 <nicolas.e.paz@outlook.com>
2021-01-08 09:02:39 -0300

    primer comentario

---
+++ src/UnaClase.java
@@ -0,0 +1,18 @@
+
+public class UnaClase {
+	
+	public static void main(String arg[])
+	{
+		String cadena1 = "Me dijeron ";
+		String cadena2 = "que habia ";
+		String cadena3 = "que ";
+		String cadena4 = "unir una cadena";
+		
+		System.out.println(cadena1+cadena2+cadena3+cadena4);
+		
+		
+		
+		
+		
+	}
+}
